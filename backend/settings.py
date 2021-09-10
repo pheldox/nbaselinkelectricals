2d5b9c2e1f24237996c3bf56d9ef9facf95ace85
pheldox <felixaryee@gmail.com>
2021-09-10 01:17:41 +0000

    allow static files-4

--- backend/settings.py
+++ backend/settings.py
@@ -18,7 +18,7 @@ import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-x^$%2l7tr6n!5m4=3kb@__y13qj4*7e--5@-b!7o)v4u%tlu(z'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
