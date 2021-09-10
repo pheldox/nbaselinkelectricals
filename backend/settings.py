213410aec06f37e4b742c4265eed8eef230c0c6c
pheldox <felixaryee@gmail.com>
2021-09-10 02:12:27 +0000

    allow static files-6

--- backend/settings.py
+++ backend/settings.py
@@ -175,7 +175,7 @@ STATICFILES_DIRS = [
     BASE_DIR / 'static',
     BASE_DIR / 'frontend/build/static',
 ]
-# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles',)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles',)
 
 MEDIA_ROOT = 'static/images'
 
